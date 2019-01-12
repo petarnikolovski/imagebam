48069400a64ef5c8994b32c3cfdd9ae72eb4119b
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2019-01-12 16:57:27 +0100

    Complete oAuth 1.0 authorization

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # ImageBam
 
-This project is not affiliated with the `imagebam.com`. This is wrapper library around their [official API][1].
+This project is not affiliated with the `imagebam.com`. This is wrapper library around their [official API][1]. This is only a proof of concept project, and not a full library. It only implements verification (which is usually the hardest part of the library), and one function (to fetch all images from owns gallery).
+
+## How to use this library?
+
+Here is the example usage:
+
+from imagebam.imagebam import ImageBam
+
+```python
+consumer_key = 'REGISTER_YOUR_APP_TO_OBTAIN_THIS'
+consumer_secret = 'REGISTER_YOUR_APP_TO_OBTAIN_THIS'
+
+ib = ImageBam(consumer_key, consumer_secret)
+ib.set_fake_user_agent()  # If you don't want to appear as robot
+
+# This is workflow to obtain the token
+token, secret = ib.obtain_unathorized_token()
+ib.authorize_token(token)  # Opens up a browser, asking for permission and giving authorization code
+access_token, access_token_secret = ib.obtain_access_token(
+  token,
+  secret,
+  'AUTHORIZATION_CODE_OBTAINED_FROM_BROWSER'
+)
+
+# Now use some API methods
+ib.fetch_list_of_images_from_gallery(access_token, access_token_secret, 'YOUR_GALLERY_ID')
+```
 
 [1]: https://code.google.com/archive/p/imagebam-api/
--- imagebam/imagebam.py
+++ imagebam/imagebam.py
@@ -2,6 +2,7 @@ import uuid
 import time
 import hashlib
 import requests
+import webbrowser
 from fake_useragent import UserAgent
 
 
@@ -76,3 +77,121 @@ class ImageBam:
         response = r.text
         creds = dict(tuple(e.split('=')) for e in response.split('&'))
         return (creds['oauth_token'], creds['oauth_token_secret'])
+
+    def authorize_token(self, oauth_token):
+        """
+        Redirect user to obtain authorization code.
+
+        :param oauth_token: Token obatined from the imagebam (see
+            :func:`ImageBam.obtain_unathorized_token`)
+        :type oauth_token: string
+
+        This should open a tab in the browser, asking you to accept app
+        and copy authorization code.
+        """
+        url = 'http://www.imagebam.com/sys/oauth/authorize_token'
+        webbrowser.open(''.join([url, '?oauth_token=', oauth_token]), new=2)
+
+    def obtain_access_token(
+        self, oauth_token, oauth_token_secret, oauth_verifier):
+        """
+        Obtain access token for the application.
+
+        :param oauth_token: Token obatined from the imagebam (see
+            :func:`ImageBam.obtain_unathorized_token`)
+        :type oauth_token: string
+        :param oauth_token_secret: Token secret obtained from imagebam (see
+            :func:`ImageBam.obtain_unathorized_token`)
+        :type oauth_token_secret: string
+        :param oauth_verifier: Autorization code for the app, obtained by using
+            :func:`ImageBam.authorize_token`
+        :type oauth_verifier: string
+        :return:
+        :rtype:
+        """
+        url = 'http://www.imagebam.com/sys/oauth/request_access_token'
+
+        oauth_timestamp = int(time.time())
+        oauth_nonce = str(uuid.uuid4())
+
+        oauth_signature = self.generate_oauth_signature(
+            self.key,
+            self.secret,
+            oauth_timestamp,
+            oauth_nonce,
+            oauth_token,
+            oauth_token_secret
+        )
+
+        payload = {
+            'oauth_consumer_key': self.key,
+            'oauth_nonce': oauth_nonce,
+            'oauth_signature': oauth_signature,
+            'oauth_signature_method': 'MD5',
+            'oauth_timestamp': oauth_timestamp,
+            'response_format': 'JSON',
+            'oauth_token': oauth_token,
+            'oauth_verifier': oauth_verifier,
+        }
+
+        r = requests.get(url, params=payload, headers=self.headers)
+        response = r.text
+        creds = dict(tuple(e.split('=')) for e in response.split('&'))
+        return (creds['oauth_token'], creds['oauth_token_secret'])
+
+    def fetch_list_of_images_from_gallery(
+        self, oauth_token, oauth_token_secret, gallery_id):
+        """
+        :param oauth_token: Token obatined from the imagebam (see
+            :func:`ImageBam.obtain_access_token`)
+        :type oauth_token: string
+        :param oauth_token_secret: Token secret obtained from imagebam (see
+            :func:`ImageBam.obtain_access_token`)
+        :type oauth_token_secret: string
+        :param gallery_id: ID of a gallery, ID is located in the gallery url
+            e.g. imagebam.com/gallery/ID
+        :type gallery_id: string
+        :return: List of image urls
+        :rtype: list of strings
+        """
+        url = 'http://www.imagebam.com/sys/API/resource/get_gallery_images'
+
+        oauth_timestamp = int(time.time())
+        oauth_nonce = str(uuid.uuid4())
+
+        oauth_signature = self.generate_oauth_signature(
+            self.key,
+            self.secret,
+            oauth_timestamp,
+            oauth_nonce,
+            oauth_token,
+            oauth_token_secret
+        )
+
+        payload = {
+            'oauth_consumer_key': self.key,
+            'oauth_nonce': oauth_nonce,
+            'oauth_signature': oauth_signature,
+            'oauth_signature_method': 'MD5',
+            'oauth_timestamp': oauth_timestamp,
+            'response_format': 'JSON',
+            'oauth_token': oauth_token,
+            'gallery_id': gallery_id
+        }
+
+        r = requests.get(url, params=payload, headers=self.headers)
+        # If user is not the owner of the gallery, then they cannot access the
+        # resource. Example response for failed access:
+        # {
+        #     "rsp":
+        #         {
+        #             "status": "fail",
+        #             "error_code": 108,
+        #             "error_message": "permission denied: gallery_id"
+        #         }
+        # }
+        rsp = r.json()['rsp']
+        if rsp['status'] == 'ok':
+            return r.json()
+        else:
+            raise Exception(rsp['error_message'])
